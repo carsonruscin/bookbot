a0e6c29199e115c6afedbb5422cbd9bb2a2236b0
Carson R <carsonruscin@gmail.com>
2025-04-01 21:46:24 -0500

    added function to sort characters by count and format a report

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from stats import get_num_of_words, get_book_text, get_character_count
-
+import sys
+from stats import get_num_of_words, get_book_text, get_character_count, sort_characters_by_count, formatted_report
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-    with open("books/frankenstein.txt") as f:
-        text = get_book_text(f)
-        num_of_words = get_num_of_words(text)
-        character_count = get_character_count(text)
-
-        print(f"There are {num_of_words} words in this text "
-              f"and each unique character and its count are listed here! \n{character_count}")
+    book_path = sys.argv[1]
+    try:
+        with open(book_path) as f:
+            text = get_book_text(f)
+            num_of_words = get_num_of_words(text)
+            character_count = get_character_count(text)
+            sorted_characters = sort_characters_by_count(character_count)
+            formatted_report(book_path, num_of_words, sorted_characters)
+    except FileNotFoundError:
+        print(f"Error: The file '{book_path}' was not found.")
+        sys.exit(1)
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,4 @@
 
-
 def get_book_text(filepath):
     file_contents = filepath.read()
     return file_contents
@@ -19,3 +18,25 @@ def get_character_count(book_text):
             count_dict[lowercase_char] = 1
 
     return count_dict
+
+def sort_characters_by_count(character_counts):
+    char_list = []
+    for char, count in character_counts.items():
+        if char.isalpha():
+            char_list.append({"char": char, "count": count})
+
+    def get_count(item):
+        return item["count"]
+
+    char_list.sort(reverse=True, key=get_count)
+    return char_list
+
+def formatted_report(book_path, word_count, sorted_char_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count -----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count ---------")
+    for item in sorted_char_list:
+        print(f"{item['char']}: {item['count']}")
+    print("============= END =============")
