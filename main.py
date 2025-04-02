1cfbb1d436bb8fbf9a6d22eed24b15a791d2111f
Carson R <carsonruscin@gmail.com>
2025-04-01 20:10:41 -0500

    refactored, changed .gitignore

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+from stats import get_num_of_words
+
+
 def get_book_text(filepath):
     file_contents = filepath.read()
     return file_contents
 
-def get_num_of_words(book_text):
-    words = book_text.split()
-    return len(words)
 
 def main():
     with open("books/frankenstein.txt") as f:
@@ -12,7 +12,6 @@ def main():
         num_of_words = get_num_of_words(text)
 
         print(f"{num_of_words} words found in the document")
-
 
 
 main()
---
+++ stats.py
@@ -0,0 +1,5 @@
+
+
+def get_num_of_words(book_text):
+    words = book_text.split()
+    return len(words)
